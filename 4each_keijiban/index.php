d0697c34cd438b5eae6d00d867401bb70b417d35
uki-sa <yuki0906sato@gmail.com>
2019-09-25 19:59:30 +0900

    none

---
+++ 4each_keijiban/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>4eachblog 掲示板</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+    
+<body>
+ <div class="logo">
+  <img src="4eachblog_logo.jpg">
+  </div>
+  <header>
+   <ul>
+    <li>トップ</li>  
+    <li>プロフィール</li>
+    <li>4eachについて</li>
+    <li>書籍フォーム</li>
+    <li>問い合わせ</li>
+    <li>その他</li>
+   </ul>
+  </header>
+     
+  <main>
+   <div class="left">
+    <h1>プログラミングに役立つ掲示板</h1>
+    <form method="post" action="insert.php">
+      <p>入力フォーム</p>
+     <div>
+         <label>ハンドルネーム</label>
+         <br>
+         <input type="text" class="text" size="35" name="handlename">
+        </div>
+     <div>
+      <label>タイトル</label>
+      <br>
+      <input type="text" class="text" size="35" name="title">
+        </div>
+    <div>
+         <label>コメント</label>
+         <br>
+         <textarea cols="35" rows="7" name="comments"></textarea>
+        </div>
+        
+    <div>
+     <input type="submit" class="submit" value="投稿する">
+    </div>
+       </form>   
+    
+
+    <?php
+  
+  mb_internal_encoding("utf8");
+  $pdo=new PDO("mysql:dbname=4each_keijiban;host=localhost;","root","mysql");
+  
+  $stml=$pdo->query("select*from 4each_keijiban");
+    
+    
+ while($row=$stml->fetch())  { 
+  echo"<div class='kiji'>";
+   echo"<h3>".$row['title']."</h3>";
+    echo"<div class='contents'>";
+    echo $row['comments'];
+    echo "<div class='handlename'>posted by".$row['handlename']."</div>";
+       echo"</div>";
+       echo"</div>";
+       }
+       
+  ?>
+       </div>
+      
+   <div class="right">
+    <h2>人気の記事</h2>
+     <p>PHP オススメ本</p>
+     <p>PHP MyAdminの使い方</p>
+     <p>今人気のエディタ Top5</p>
+     <p>HTMLの基礎</p>
+   
+     <h2>オススメリンク</h2>
+     <p>インターノウス株式会社</p>
+     <p>XAMPRのダウンロード</p>
+     <p>Eclipseのダウンロード</p>
+     <p>Bracketsのダウンロード</p>
+       
+     <h2>カテゴリ</h2>
+     <p>HTML</p>
+     <p>PHP</p>
+     <p>MySQL</p>
+     <p>JavaScript</p>
+      
+   </div>      
+    
+<footer>
+ copyright(c) internous ｜　4each blog is the one  which provides A to Z about programming.      
+      </footer>
+      
+      
+      </main>      
+</body>
+</html>
