37054a20201be656fac46af77a2d4e2c8c9981ca
uki-sa <yuki0906sato@gmail.com>
2019-10-16 20:30:55 +0900

    none

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3a/505d24d400f000191c4ca2b66eb3a058
@@ -0,0 +1,8 @@
+package testdb;
+
+public class Test {
+	public static void main(String[]args){
+		TestUserDAO dao=new TestUserDAO();
+		dao.select("taro","123");
+	}
+}
--- testDB/src/testdb/Test.java
+++ testDB/src/testdb/Test.java
@@ -4,5 +4,7 @@ public class Test {
 	public static void main(String[]args){
 		TestUserDAO dao=new TestUserDAO();
 		dao.select("taro","123");
+		dao.selectAll();
 	}
+
 }
