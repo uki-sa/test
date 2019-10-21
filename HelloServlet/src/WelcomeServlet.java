56ba2001af25ad8e445a215bdf63a39e4bca697f
uki-sa <yuki0906sato@gmail.com>
2019-10-21 21:38:49 +0900

    10/21

--- HelloServlet/src/WelcomeServlet.java
+++ HelloServlet/src/WelcomeServlet.java
@@ -22,7 +22,7 @@ public class WelcomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html;charset=UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 		String username=request.getParameter("username");
 
 		System.out.println(username);
